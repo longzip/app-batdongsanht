24f1c5b433ed562f6cb58cf20a7e8cd8c464f96f
Lo Van Long <longlv@v-auto.com.vn>
2022-05-28 18:10:42 +0700

    first

---
+++ src/pages/PageTodo.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="q-pa-md">
+		
+		<no-tasks
+			v-if="!Object.keys(tasksTodo).length"></no-tasks>
+
+		<tasks-todo
+			v-else
+			:tasksTodo="tasksTodo" />
+
+  </q-page>
+</template>
+
+<script>
+	import { mapGetters } from 'vuex'
+
+	export default {
+		computed: {
+			...mapGetters('tasks', ['tasksTodo', 'tasksCompleted'])
+		},
+		components: {
+			'tasks-todo' : require('components/Tasks/TasksTodo.vue').default,
+			'no-tasks' : require('components/Tasks/NoTasks.vue').default
+		}
+	}
+</script>
+
+<style>
+	
+</style>
